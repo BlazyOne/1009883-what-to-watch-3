0ba5e078f2d36ff4c51f2c1d7b6386b9c616caf4
Blazy One <badjinua@gmail.com>
2020-04-27 03:14:34 +0500

    Добавил реализацию функциональности basename для рутов в const

--- src/const.js
+++ src/const.js
@@ -6,27 +6,31 @@ const FilmInfoTabTypes = {
 
 const FILM_ID_STRING_ADDITION = `film_`;
 
+const ROUTER_BASENAME = `/wtw-githubpages`;
+
+const ROUTE_PREFIX = ROUTER_BASENAME === `/` ? `` : ROUTER_BASENAME;
+
 const AppRoute = {
-  MAIN: `/`,
-  SIGN_IN: `/login`,
-  MY_LIST: `/mylist`,
-  FILM_GENERAL: `/films/:id`,
+  MAIN: ROUTER_BASENAME,
+  SIGN_IN: `${ROUTE_PREFIX}/login`,
+  MY_LIST: `${ROUTE_PREFIX}/mylist`,
+  FILM_GENERAL: `${ROUTE_PREFIX}/films/:id`,
   createParticularFilmUrl(filmId) {
     const serverFilmId = +filmId.substring(FILM_ID_STRING_ADDITION.length);
 
-    return `/films/${serverFilmId}`;
+    return `${ROUTE_PREFIX}/films/${serverFilmId}`;
   },
-  ADD_REVIEW_GENERAL: `/films/:id/review`,
+  ADD_REVIEW_GENERAL: `${ROUTE_PREFIX}/films/:id/review`,
   createParticularAddReviewUrl(filmId) {
     const serverFilmId = +filmId.substring(FILM_ID_STRING_ADDITION.length);
 
-    return `/films/${serverFilmId}/review`;
+    return `${ROUTE_PREFIX}/films/${serverFilmId}/review`;
   },
-  PLAYER_GENERAL: `/player/:id`,
+  PLAYER_GENERAL: `${ROUTE_PREFIX}/player/:id`,
   createParticularPlayerUrl(filmId) {
     const serverFilmId = +filmId.substring(FILM_ID_STRING_ADDITION.length);
 
-    return `/player/${serverFilmId}`;
+    return `${ROUTE_PREFIX}/player/${serverFilmId}`;
   }
 };
 
